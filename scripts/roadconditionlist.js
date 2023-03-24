c1df1ee9f871ab059694fb40fb131040b710ba01
Charlie Zhang <czhang177@my.bcit.ca>
2023-03-24 10:37:25 -0700

    Remove unwanted fields in card

--- scripts/roadconditionlist.js
+++ scripts/roadconditionlist.js
@@ -26,10 +26,10 @@ function populateRoadConditionList() {
                 var type = doc.data().type; //gets the type field
                 var upvotes = doc.data().likes; //gets the amount of likes
                 var downvotes = doc.data().dislikes; //gets the amount of dislikes
-                var city = doc.data().city;
+                // var city = doc.data().city;
                 var address = doc.data().address;
-                var latitude = doc.data().latitude;
-                var longitude = doc.data().longitude;
+                // var latitude = doc.data().latitude;
+                // var longitude = doc.data().longitude;
                 var description = doc.data().description; //gets the description field
                 // var time = doc.data().timestamp.toDate();
                 console.log(title)
@@ -45,10 +45,10 @@ function populateRoadConditionList() {
                 roadConditionCard.querySelector('.downvotes').innerHTML = downvotes;
                 roadConditionCard.querySelector('.downvoteIcon').setAttribute("type", `${cardCounter}`)
                 roadConditionCard.querySelector('.downvoteIcon').onclick = () => downvote(doc.id, homeCity);
-                roadConditionCard.querySelector('.city').innerHTML = city;
+                // roadConditionCard.querySelector('.city').innerHTML = city;
                 roadConditionCard.querySelector('.address').innerHTML = address;
-                roadConditionCard.querySelector('.latitude').innerHTML = `Latitude: ${latitude}`;
-                roadConditionCard.querySelector('.longitude').innerHTML = `Longitude: ${longitude}`;
+                // roadConditionCard.querySelector('.latitude').innerHTML = `Latitude: ${latitude}`;
+                // roadConditionCard.querySelector('.longitude').innerHTML = `Longitude: ${longitude}`;
                 roadConditionCard.querySelector('.description').innerHTML = `Description: ${description}`;
                 // reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();    //equiv getElementByClassName
 
